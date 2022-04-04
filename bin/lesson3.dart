955cd76fe1c9563a6c850c8f76eafba3c275b91b
askaroff939 <fayzullo.asqarov@gmail.com>
2022-04-04 17:36:37 +0500

    commit massage

---
+++ bin/lesson3.dart
@@ -0,0 +1,26 @@
+class Reptile{
+  void bite() => print("Chomp");
+  void swim() => print("Swimming");
+  void crawl() => print("Crawling");
+
+  void hunt(String name) {
+    print("$runtimeType....");
+    bite();
+    swim();
+    crawl();
+    print("eat $name");
+  }
+}
+
+
+class Alligator extends Reptile{}
+
+class Crocodile extends Reptile{}
+
+void main() {
+  Alligator alligator = Alligator();
+  Crocodile crocodile = Crocodile();
+
+  alligator.hunt("Baliq");
+  crocodile.hunt("Zebra");
+}
